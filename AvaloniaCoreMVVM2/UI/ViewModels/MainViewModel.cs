225089421afae4bbfc10c3130ce9dc79eb82e138
Popelenkov Vladlen <popelenkow@yandex.ru>
2017-10-06 18:27:41 +0700

    Init

---
+++ AvaloniaCoreMVVM2/UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RSL.UI.MVVM;
+
+namespace RSL.UI.ViewModels
+{
+    class MainViewModel : BindableBase
+    {
+
+        public MainViewModel()
+        {
+            NavCommand = new MyCommand<string>(OnNav);
+        }
+
+        private CustomerListViewModel _custListViewModel = new CustomerListViewModel();
+
+        private OrderViewModel _orderViewModelModel = new OrderViewModel();
+
+        private BindableBase _currentViewModel;
+
+        public BindableBase CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set { SetProperty(ref _currentViewModel, value); }
+        }
+
+        public MyCommand<string> NavCommand { get; private set; }
+
+        private void OnNav(string destination)
+        {
+
+            switch (destination)
+            {
+                case "orders":
+                    CurrentViewModel = _orderViewModelModel;
+                    break;
+                case "customers":
+                default:
+                    CurrentViewModel = _custListViewModel;
+                    break;
+            }
+        }
+    }
+}
---
+++ AvaloniaCoreMVVM2/UI/ViewModels/OrderViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RSL.UI.MVVM;
+
+namespace RSL.UI.ViewModels
+{
+    class OrderViewModel : BindableBase
+    {
+    }
+}
